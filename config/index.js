09a1f00542f0eb5ac3679abf4113e63a4f001a32
WuDiXiaoShuaiCai <876991244@qq.com>
2018-07-09 11:30:18 +0800

    proxyTable代理和跨域

--- config/index.js
+++ config/index.js
@@ -11,17 +11,20 @@ module.exports = {
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {
-        '*':{
-            target: 'http://localhost:3000/',
-            filter: function(pathname, req){
-                return pathname.match('^/api');
-            },
-            changeOrigin: true,
+        '/api':{
+            target: 'http://localhost:3000/', // 接口的域名
+            
+             // secure: false,  // 如果是https接口，需要配置这个参数
+            // changeOrigin: true, // 如果接口跨域，需要进行这个参数配置
             pathRewrite: {
                 '^/api': '',
             }
         }
     },
+    //注意： '/api' 为匹配项，target 为被请求的地址，因为在 ajax 的 url 中加了前缀 '/api'，
+    //而原本的接口是没有这个前缀的，所以需要通过 pathRewrite 来重写地址，将前缀 '/api' 转为 ''
+    //如果本身的接口地址就有 '/api' 这种通用前缀，就可以把 pathRewrite 删掉。
+
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
